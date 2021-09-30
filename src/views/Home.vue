cb750684e2a23f5f7755c27f1884fe972f83eb00
Mhret Moges <andreasjhon488@gmail.com>
2021-09-30 09:22:38 +0300

    Vite, Tailwind and Vue-Router Boilerplate

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>Home</div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      trendingGiphies: null,
+    };
+  },
+  mounted() {
+    this.getTrendingGiphies();
+  },
+  methods: {
+    async getTrendingGiphies() {
+      try {
+        axios
+          .get(
+            'https://api.giphy.com/v1/gifs/trending?api_key=dv7bOJvb8GOZ0Y5W6l2coTs9rt9i1MZM'
+          )
+          .then((response) => {
+            console.log(response.data.data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
